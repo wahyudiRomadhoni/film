ecc101c9b14cb14c6e2e2611286a0d82fd22e7c3
wahyudiRomadhoni <wahyudiromadoni69@gmail.com>
2024-07-29 12:43:00 +0700

    lengkapnya ya gess

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var link_popular = "https://api.themoviedb.org/3/movie/popular?api_key=241213c1fea22c0b49c61e53273561e9"
 var link_kategori = "https://api.themoviedb.org/3/genre/movie/list?api_key=241213c1fea22c0b49c61e53273561e9"
-var link_content = "https://api.themoviedb.org/3/discover/movie?api_key=241213c1fea22c0b49c61e53273561e9&with_genres=35"
+var link_content = "https://api.themoviedb.org/3/discover/movie?api_key=241213c1fea22c0b49c61e53273561e9&with_genres="
 var gambar = "https://image.tmdb.org/t/p/w300"
 var populer = document.getElementById("film-populer")
 
@@ -18,30 +18,39 @@ fetch(link_popular).then(r => r.json())
 // end
 var input_filter = document.getElementById("filter");
 var button_filter = document.getElementById("button-filter");
+var movies = document.getElementById("movies");
 
 
 // fetch kategori data
-fetch(link_kategori).then(r => r.json())
-.then((response)=> {
-    var genres = response.genres;
-    genres.map(genre => {
-        input_filter.innerHTML += `<option>${genre.name}</option>`
+function getKategori(id) {
+    fetch(link_kategori).then(r => r.json())
+    .then((response)=> {
+        var genres = response.genres;
+        genres.map(genre => {
+            input_filter.innerHTML += `<option value="${genre.id}">${genre.name}</option>`
+        })
     })
-})
+}
 
 // fetch content by kategori
-fetch(link_content).then(r => r.json())
-.then((response)=> {
-    var results = response.results;
-    results.map(result => {
-        movies.innerHTML += `<img src="${gambar + result.poster_path}"/>`
+function getContentKategori(id) {
+    fetch(link_content + id).then(r => r.json())
+    .then((response)=> {
+        var results = response.results;
+        results.map(result => {
+            movies.innerHTML += `<img src="${gambar + result.poster_path}"/>`
+        })
     })
-})
+}
+
+function tampilkanFilter(){
+    var value_filter = input_filter.value
+    movies.innerHTML = ''
+    getContentKategori(value_filter)
+}
 
 
+button_filter.addEventListener("click", tampilkanFilter);
 
-
-// button_filter.addEventListener("click", function() {
-//     var value_filter = input_filter.value
-//     alert(value_filter)
-// })
+getKategori()
+getContentKategori("28")
